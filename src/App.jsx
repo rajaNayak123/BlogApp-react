4c7ae66bdaaa4bf2b5a73d58abda73f3b8ceebde
rajaNayak123 <nayakraja151@gmail.com>
2024-06-06 17:57:50 +0530

    auth serv.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // console.log(process.env.REACT_APP_APPWRITE_URL);  // For if i create application create-react-app
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
-    
+      <h1>Blogging app</h1>
     </>
   )
 }
---
+++ src/appwriteSer/auth.js
@@ -0,0 +1,61 @@
+import conf from "../conf/conf";
+import { Client, ID, Account } from "appwrite";
+
+export class AuthService {
+    clint = new clint();
+    account;
+
+    constructor() {
+        this.clint
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.clint);
+    }
+    // SignUp or when a user create first time their account
+    async createAccount({ name, email, password }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), name, email, password);
+            if (userAccount) {
+                // call another method
+                return this.login({ email, password });
+            } else {
+                return null;
+            }
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+
+    // if user already created the account now he want to login
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // I want to know the user account exitst or not
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("getCurrentUser:: error: " + error);
+        }
+        return null;
+    }
+
+    // how to delete user account or Logtout
+    async logout() {
+        try {
+           await this.account.deleteSession('current')
+        } catch (error) {
+            console.log("logout:: error: " + error);
+        }
+    }
+};
+
+const authService = new AuthService();
+
+export default authService;
