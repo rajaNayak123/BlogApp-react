94f611ce00f2808c160b76524694a15dd358ce41
rajaNayak123 <nayakraja151@gmail.com>
2024-06-16 16:35:50 +0530

    add inputField and buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import { Container, Logo, Logout, LogoutBtn } from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+  const navItems = [
+    {
+      name: "Home",
+      slug: '/',
+      Active: true,
+    },
+    {
+      name: "Login",
+      slug: '/login',
+      Active: !authStatus
+    },
+    {
+      name: "Signup",
+      slug: '/signup',
+      Active: !authStatus
+    },
+    {
+      name: "All Posts",
+      slug: '/all-posts',
+      Active: authStatus
+    },
+    {
+      name: "Add posts",
+      slug: "/add-posts",
+      Active: authStatus
+    }
+  ]
+
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {
+              navItems.map((item) =>
+                item.Active ? (
+                  <li key={item.name}>
+                    <button
+                      onClick={() => navigate(item.slug)}
+                      className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                    >{item.name}</button>
+                  </li>
+                ) : null
+              )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import authService from '../../appwriteSer/auth'
+import { useDispatch } from 'react-redux'
+import { logout } from '../../store/storeSlice'
+const LogoutBtn = () => {
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authService.logout()
+            .then(() => {
+                dispatch(logout())
+            })
+            .catch(() => { });
+    }
+    return (
+        <button
+            className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+        >Logout</button>
+    )
+}
+
+export default LogoutBtn
